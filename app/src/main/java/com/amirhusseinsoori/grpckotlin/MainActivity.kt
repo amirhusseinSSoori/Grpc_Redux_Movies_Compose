a0511b7106aaf3e882b2dbadfb48c15d1d094e81
Amirhussein soori <amirhusseinsppro75@gmail.com>
2022-02-16 00:10:55 +0330

    add compose in gradle

--- app/src/main/java/com/amirhusseinsoori/grpckotlin/MainActivity.kt
+++ app/src/main/java/com/amirhusseinsoori/grpckotlin/MainActivity.kt
@@ -1,28 +1,13 @@
 package com.amirhusseinsoori.grpckotlin
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-
 import dagger.hilt.android.AndroidEntryPoint
-import io.grpc.ManagedChannel
-import io.grpc.ManagedChannelBuilder
-import io.grpc.StatusRuntimeException
 
-import io.grpc.stub.StreamObserver
-import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
-
-import java.util.logging.Level
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
-
     }
 }
