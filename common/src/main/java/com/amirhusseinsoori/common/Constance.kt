5438f08f768cebb4470ff045307d1a7f1cf3026c
amirhusseinSSoori <amirhusseinsppro75@gmail.com>
2022-08-02 01:20:30 +0430

    update gradle(7.2.0) and dependencies

--- common/src/main/java/com/amirhusseinsoori/common/Constance.kt
+++ common/src/main/java/com/amirhusseinsoori/common/Constance.kt
@@ -1,7 +1,7 @@
 package com.amirhusseinsoori.common
 
 object Constance {
-    const val BaseIp = "192.168.172.147"
+    const val BaseIp = "192.168.94.147"
     const val BasePort = 50051
     const val NoError ="NoError"
 }
