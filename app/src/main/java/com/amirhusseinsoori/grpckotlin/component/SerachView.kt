7bee90d435b6bfa532274cb394be9374952c9824
amirhusseinSSoori <amirhusseinsppro75@gmail.com>
2022-11-12 14:53:14 +0330

    add Search view Component

---
+++ app/src/main/java/com/amirhusseinsoori/grpckotlin/component/SerachView.kt
@@ -0,0 +1,190 @@
+package com.amirhusseinsoori.grpckotlin.component
+
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.amirhusseinsoori.grpckotlin.R
+import com.amirhusseinsoori.grpckotlin.ui.theme.ThemeGrpcMovies
+
+@Composable
+fun ExpandableSearchView(
+    searchDisplay: String,
+    onSearchDisplayChanged: (String) -> Unit,
+    onSearchDisplayClosed: () -> Unit,
+    modifier: Modifier = Modifier,
+    expandedInitially: Boolean = false,
+    tint: Color = MaterialTheme.colors.onPrimary
+) {
+    val (expanded, onExpandedChanged) = remember {
+        mutableStateOf(expandedInitially)
+    }
+
+
+    Crossfade(targetState = expanded) { isSearchFieldVisible ->
+        when (isSearchFieldVisible) {
+            true -> ExpandedSearchView(
+                searchDisplay = searchDisplay,
+                onSearchDisplayChanged = onSearchDisplayChanged,
+                onSearchDisplayClosed = onSearchDisplayClosed,
+                onExpandedChanged = onExpandedChanged,
+                modifier = modifier,
+                tint = tint
+            )
+
+            false -> CollapsedSearchView(
+                onExpandedChanged = onExpandedChanged,
+                modifier = modifier,
+                tint = tint
+            )
+        }
+    }
+}
+
+@Composable
+fun SearchIcon(iconTint: Color) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_search),
+        contentDescription = "search icon",
+        tint = iconTint
+    )
+}
+
+@Composable
+fun CollapsedSearchView(
+    onExpandedChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+    tint: Color = MaterialTheme.colors.onPrimary,
+) {
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 2.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "Tasks",
+            style = MaterialTheme.typography.h6,
+            modifier = Modifier
+                .padding(start = 16.dp)
+        )
+        IconButton(onClick = { onExpandedChanged(true) }) {
+            SearchIcon(iconTint = tint)
+        }
+    }
+}
+
+@Composable
+fun ExpandedSearchView(
+    searchDisplay: String,
+    onSearchDisplayChanged: (String) -> Unit,
+    onSearchDisplayClosed: () -> Unit,
+    onExpandedChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+    tint: Color = MaterialTheme.colors.onPrimary,
+) {
+    val focusManager = LocalFocusManager.current
+
+    val textFieldFocusRequester = remember { FocusRequester() }
+
+    SideEffect {
+        textFieldFocusRequester.requestFocus()
+    }
+
+    var textFieldValue by remember {
+        mutableStateOf(TextFieldValue(searchDisplay, TextRange(searchDisplay.length)))
+    }
+
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(onClick = {
+            onExpandedChanged(false)
+            onSearchDisplayClosed()
+        }) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_back),
+                contentDescription = "back icon",
+                tint = tint
+            )
+        }
+        TextField(
+            value = textFieldValue,
+            onValueChange = {
+                textFieldValue = it
+                onSearchDisplayChanged(it.text)
+            },
+            trailingIcon = {
+                SearchIcon(iconTint = tint)
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .focusRequester(textFieldFocusRequester),
+            label = {
+                Text(text = "Search", color = tint)
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    focusManager.clearFocus()
+                }
+            )
+        )
+    }
+}
+
+@Preview
+@Composable
+fun CollapsedSearchViewPreview() {
+    ThemeGrpcMovies {
+        Surface(
+            color = MaterialTheme.colors.primary
+        ) {
+            ExpandableSearchView(
+                searchDisplay = "",
+                onSearchDisplayChanged = {},
+                onSearchDisplayClosed = {}
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ExpandedSearchViewPreview() {
+    ThemeGrpcMovies {
+        Surface(
+            color = MaterialTheme.colors.primary
+        ) {
+            ExpandableSearchView(
+                searchDisplay = "",
+                onSearchDisplayChanged = {},
+                expandedInitially = true,
+                onSearchDisplayClosed = {}
+            )
+        }
+    }
+}
--- common/src/main/java/com/amirhusseinsoori/common/Constance.kt
+++ common/src/main/java/com/amirhusseinsoori/common/Constance.kt
@@ -1,7 +1,7 @@
 package com.amirhusseinsoori.common
 
 object Constance {
-    const val BaseIp = "192.168.214.147"
+    const val BaseIp = "192.168.220.147"
     const val BasePort = 50051
     const val NoError ="NoError"
 }
