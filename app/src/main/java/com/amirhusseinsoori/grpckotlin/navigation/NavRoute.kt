c43cbbb2f7b516938638256b9087dbf1db239ab1
amirhusseinSSoori <amirhusseinsppro75@gmail.com>
2023-02-09 13:27:27 +0330

    add SearchRoute this Nav Route

--- app/src/main/java/com/amirhusseinsoori/grpckotlin/navigation/NavRoute.kt
+++ app/src/main/java/com/amirhusseinsoori/grpckotlin/navigation/NavRoute.kt
@@ -5,4 +5,5 @@ import androidx.navigation.NamedNavArgument
 sealed class NavRoute(var route: String, val arguments: List<NamedNavArgument>) {
     object IntroRoute : NavRoute("Intro_screen", emptyList())
     object MainRoute : NavRoute("Main_screen", emptyList())
+    object SearchRoute : NavRoute("Search_screen", emptyList())
 }
--- common/src/main/java/com/amirhusseinsoori/common/Constance.kt
+++ common/src/main/java/com/amirhusseinsoori/common/Constance.kt
@@ -1,7 +1,7 @@
 package com.amirhusseinsoori.common
 
 object Constance {
-    const val BaseIp = "192.168.1.102"
+    const val BaseIp = "192.168.8.104"
     const val BasePort = 50051
     const val NoError ="NoError"
 }
