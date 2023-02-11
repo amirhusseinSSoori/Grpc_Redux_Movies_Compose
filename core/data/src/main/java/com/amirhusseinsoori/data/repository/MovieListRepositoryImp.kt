f80a6e9dcc5379d735faff6e7bdb6c5a2b0d9204
amirhusseinSSoori <amirhusseinsppro75@gmail.com>
2023-02-11 23:17:44 +0330

    remove comment and logs

--- core/data/src/main/java/com/amirhusseinsoori/data/repository/MovieListRepositoryImp.kt
+++ core/data/src/main/java/com/amirhusseinsoori/data/repository/MovieListRepositoryImp.kt
@@ -1,14 +1,11 @@
 package com.amirhusseinsoori.data.repository
 
 
-import android.util.Log
 import arrow.core.Either
 import com.amirhusseinsoori.data.dataSource.category.MovieRemoteSource
 import com.amirhusseinsoori.domain.entity.DomainMoviesItem
 import com.amirhusseinsoori.domain.repository.MovieListRepository
 import com.amirhusseinsoori.data.mapper.moviesMapToDomain
-
-
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
 
@@ -20,21 +17,11 @@ class MovieListRepositoryImp @Inject constructor(var local: MovieRemoteSource) :
         flow<List<DomainMoviesItem>> {
             emit(local.getAllMovie(type).videoListXList.moviesMapToDomain())
         }.flowOn(Dispatchers.IO)
-
     override fun searchMovies(type: String): Flow<Either<List<DomainMoviesItem>, Throwable>> =
         flow<Either<List<DomainMoviesItem>, Throwable>> {
-            Log.e("ssssss", "searchMovies:${local.searchMovies(type).videoListXList} ", )
             emit(Either.Left(local.searchMovies(type).videoListXList.moviesMapToDomain()))
-        }.catch {
-                ex ->
-            Log.e("ssssss", "searchMovies:${ex.message} ", )
+        }.catch { ex ->
             emit(Either.Right(ex))
         }.flowOn(Dispatchers.IO)
-
-
-//    =
-//    flow<List<DomainMoviesItem>> {
-//        emit(local.searchMovies(type).videoListXList.moviesMapToDomain())
-//    }.flowOn(Dispatchers.IO)
 
 }
