899c8592f334c65d24aa1e9d264fe3ce4a70795d
amirhusseinSSoori <amirhusseinsppro75@gmail.com>
2023-02-11 23:15:50 +0330

    complite Search Screen

--- app/src/main/java/com/amirhusseinsoori/grpckotlin/ui/search/SearchScreen.kt
+++ app/src/main/java/com/amirhusseinsoori/grpckotlin/ui/search/SearchScreen.kt
@@ -5,29 +5,44 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import com.amirhusseinsoori.grpckotlin.component.banner.utils.lifecycleOwnerTools
+import com.amirhusseinsoori.grpckotlin.ui.MovieViewModel
+import com.amirhusseinsoori.grpckotlin.ui.ShowErrorDialog
+import com.amirhusseinsoori.grpckotlin.ui.ShowLoading
+import com.amirhusseinsoori.grpckotlin.ui.movie.pattern.MovieEffect
+import com.amirhusseinsoori.grpckotlin.ui.movie.pattern.MovieViewState
+import com.amirhusseinsoori.grpckotlin.ui.search.component.SearchItem
 
 
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalAnimationApi::class)
 @Composable
-fun Search() {
+fun Search(viewModel: MovieViewModel) {
+
+
     Column(
         modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         var expanded by remember { mutableStateOf(false) }
         var value by remember { mutableStateOf("") }
+        val state by lifecycleOwnerTools(viewModel.viewState).collectAsState(MovieViewState())
+
         Surface() {
+
             Column(modifier = Modifier.fillMaxSize()) {
                 SearchBar(
                     query = value,
                     onQueryChange = {
                         value = it
+                        viewModel.searchMovies(value)
                     },
                     onSearchFocusChange = { expanded = false },
                     onClearQuery = {
@@ -35,8 +50,28 @@ fun Search() {
                     },
                     enableClose = value != "",
                 )
+                state.let { result ->
+                    result.apply {
+                        LazyColumn {
+                            items(result.searchList) { item ->
+                                SearchItem(movie = item)
+                            }
+                        }
+
+                        ShowErrorDialog(showDialog = DiaLog, message, callEvent = {
+                            viewModel.callEvent()
+                        })
+                    }
+
+
+                }
             }
 
+            viewModel.viewEffect.collectAsState(initial = MovieEffect()).let {
+                it.value.apply {
+                    ShowLoading(showProgressBar)
+                }
+            }
         }
 
     }
