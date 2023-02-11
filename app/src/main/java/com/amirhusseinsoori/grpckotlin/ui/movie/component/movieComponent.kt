a8d9acd37660d8ab681f0514a0fa8eb275a95ed4
amirhusseinSSoori <amirhusseinsppro75@gmail.com>
2023-02-11 23:25:27 +0330

    create MoveToolbar

--- app/src/main/java/com/amirhusseinsoori/grpckotlin/ui/movie/component/movieComponent.kt
+++ app/src/main/java/com/amirhusseinsoori/grpckotlin/ui/movie/component/movieComponent.kt
@@ -3,19 +3,25 @@ package com.amirhusseinsoori.grpckotlin.ui.movie.component
 import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.amirhusseinsoori.domain.entity.DomainMoviesItem
 import com.amirhusseinsoori.domain.entity.model.BannerModel
 import com.amirhusseinsoori.grpckotlin.component.banner.BannerPager
 import com.amirhusseinsoori.grpckotlin.component.banner.utils.ProvideGradiant
+import com.amirhusseinsoori.grpckotlin.ui.theme.Neutral0
 
 
 @Composable
@@ -24,7 +30,6 @@ fun MovieDetails(
     famousItems: List<DomainMoviesItem>,
     serialsItems: List<DomainMoviesItem>,
     comedyItems: List<DomainMoviesItem>,
-    searchList: List<DomainMoviesItem>
 ) {
     Banner(itemsBanner)
 
@@ -49,6 +54,34 @@ fun MovieDetails(
 
 
 }
+
+@Composable
+fun MovieToolbar(onClick:()->Unit) {
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+        val (icon, txt) = createRefs()
+        Icon(
+            modifier = Modifier.clickable { onClick() }.constrainAs(icon) {
+                top.linkTo(parent.top, margin = 4.dp)
+                end.linkTo(parent.end, margin = 16.dp)
+                bottom.linkTo(parent.bottom, margin = 4.dp)
+            },
+            imageVector = Icons.Outlined.Search,
+            tint = Neutral0,
+            contentDescription = "search"
+        )
+
+        Text(
+            modifier = Modifier.constrainAs(txt) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+                bottom.linkTo(parent.bottom)
+            },
+            text = "Movie with Grpc",
+            color = Neutral0
+        )
+    }
+}
+
 
 
 @Composable
