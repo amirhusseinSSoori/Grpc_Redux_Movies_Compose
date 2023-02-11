c5bbcbd68463026afe9299be1ccaa3b2786710c4
amirhusseinSSoori <amirhusseinsppro75@gmail.com>
2023-02-11 23:47:57 +0330

    add utilFont font for texts

---
+++ app/src/main/java/com/amirhusseinsoori/grpckotlin/component/banner/utils/Fonts.kt
@@ -0,0 +1,16 @@
+package com.amirhusseinsoori.grpckotlin.component.banner.utils
+
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.amirhusseinsoori.grpckotlin.R
+
+val utilFont = FontFamily(
+    Font(R.font.domine_bold, FontWeight.Light),
+    Font(R.font.domine_regular, FontWeight.Normal),
+    Font(R.font.montserrat_medium, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.iransanse_medium, FontWeight.Medium),
+    Font(R.font.montserrat_semibold, FontWeight.Bold)
+)
